75cd4e29f0dadf6bf0cd14303befb3b13a9421b6
meshforce1 <max.champness@students.plymouth.ac.uk>
2025-10-14 12:14:43 +0100

    added note variable
    added extra string variable for test

--- DataTypeDemo.java
+++ DataTypeDemo.java
@@ -5,12 +5,17 @@ public class DataTypeDemo {
         double temperature = 36.6;
         String name = "John";
         boolean status = true;
+	String note = "edit test";
 
         // 2. Print all variables
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Temperature: " + temperature);
         System.out.println("Status: " + status);
-
+	System.out.println("Test: " + note);
     }
 }
+
+
+
+
