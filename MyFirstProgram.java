f8f2763ae45c66bd819feffe43584b3756ac229b
meshforce1 <max.champness@students.plymouth.ac.uk>
2025-10-14 11:54:23 +0100

    created and ran successfully

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,5 +1,5 @@
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Welcome to Jave Programming!");
+    System.out.println("Welcome to Java Programming!");
   }
 }
