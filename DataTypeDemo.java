683d3790c9d23d38120e75821de75c691bcfe8f5
meshforce1 <max.champness@students.plymouth.ac.uk>
2025-10-14 12:10:59 +0100

    created and run successfully
    class to distinguish age temp name and status

---
+++ DataTypeDemo.java
@@ -0,0 +1,16 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        // 1. Declare variables of various types
+        int age = 20;
+        double temperature = 36.6;
+        String name = "John";
+        boolean status = true;
+
+        // 2. Print all variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Status: " + status);
+
+    }
+}
